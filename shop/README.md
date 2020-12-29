db7027108932f601b46f3131484715dbb263d17f
편도걸 <pdg2491@naver.com>
2020-12-29 17:24:18 +0900

    [Feat] Update React-Router
    - package.json : react-router-dom 추가
    - react-router-dom 사용하여 Router 분기

--- shop/README.md
+++ shop/README.md
@@ -1,1 +1,12 @@
 # React Study - Shop
+
+## 1. React Router
+
+* 페이지를 추가하고 싶으면 React Router Dom을 사용
+* react-router-dom
+    1. BrowserRouter vs HashRouter
+        * BrowserRouter : 기본 설정 *(서버 설정하여 잘못된 접근 페이지 설정등 필요)
+        * HashRouter    : 서버에 잘못된 정보를 요청하지 않도록 하기 위해 사용
+
+    2. index.js에서 App을 BrowserRouter등의 태그를 감싸고
+    3. Apps.js에서 Route Tag를 사용하여 URL을 라우팅하여 사용
--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Button, Jumbotron, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
+import {Route} from 'react-router-dom';
 
 function App() {
     let [shoes, setShoes] = useState(Data);
@@ -24,27 +25,44 @@ function App() {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-            <Jumbotron className="background">
-                <h1>20% Season OFF</h1>
-                <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-                </p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
-                </p>
-            </Jumbotron>
-            <div className="container">
-                <div className="row">
-                    {
-                        shoes.map((shoe) => {
-                            return (
-                                <Card shoe={shoe} key={shoe.id}/>
-                            )
-                        })
-                    }
+            <Route exact path="/">
+                <Jumbotron className="background">
+                    <h1>20% Season OFF</h1>
+                    <p>
+                        This is a simple hero unit, a simple jumbotron-style component for calling
+                        extra attention to featured content or information.
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </Jumbotron>
+                <div className="container">
+                    <div className="row">
+                        {
+                            shoes.map((shoe) => {
+                                return (
+                                    <Card shoe={shoe} key={shoe.id}/>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
+            </Route>
+            <Route path="/detail">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+                        </div>
+                        <div className="col-md-6 mt-4">
+                            <h4 className="pt-5">상품명</h4>
+                            <p>상품설명</p>
+                            <p>120000원</p>
+                            <button className="btn btn-danger">주문하기</button>
+                        </div>
+                    </div>
+                </div>
+            </Route>
         </div>
     );
 }
