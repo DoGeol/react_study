fe9f5f8a1f4d358f5940bb795289bb9d83ad8550
편도걸 <pdg2491@naver.com>
2020-12-29 17:08:35 +0900

    [Feat] Update Shop Components
    - navbar, banner, card-list 예제 추가

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Button, Jumbotron, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    let [shoes, setShoes] = useState(Data);
+    return (
+        <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Jumbotron className="background">
+                <h1>20% Season OFF</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+                </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
+            <div className="container">
+                <div className="row">
+                    {
+                        shoes.map((shoe) => {
+                            return (
+                                <Card shoe={shoe} key={shoe.id}/>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Card(props) {
+    return (
+        <div className="col-md-4">
+            <img src={'https://codingapple1.github.io/shop/shoes' + (props.shoe.id + 1) + '.jpg'}
+                 width="100%"
+                 alt={'img' + props.shoe.id}/>
+            <h4>{props.shoe.title}</h4>
+            <p>{props.shoe.content} & {props.shoe.price}</p>
+        </div>
+    )
 }
 
 export default App;
