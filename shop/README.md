1c74a682e31b112c5016b40e1ec057602c99cec5
편도걸 <pdg2491@naver.com>
2021-01-01 14:15:57 +0900

    [Feat] Update React-Router - Link,Switch,History
    - Link, Switch, History 추가

--- shop/README.md
+++ shop/README.md
@@ -10,3 +10,8 @@
 
     2. index.js에서 App을 BrowserRouter등의 태그를 감싸고
     3. Apps.js에서 Route Tag를 사용하여 URL을 라우팅하여 사용
+    4. useHistory 훅을 사용하여 뒤로가기 구현 가능
+    5. Link 훅을 사용하여 href 대신에 사용 가능  
+       `<div><Link to="/">Home</Link></div>`
+    6. Switch 훅을 사용하여 Switch 태그를 감싸면 하나의 Router만 보여줌 (중복 방지)
+        * 다만 중복 될 경우 제일 위의 라우터가 보여짐
--- shop/src/App.js
+++ shop/src/App.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import {Button, Jumbotron, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
-import {Route} from 'react-router-dom';
+import Detail from './Detail';
+import {Link, Route, Switch} from 'react-router-dom';
 
 function App() {
     let [shoes, setShoes] = useState(Data);
@@ -13,8 +14,8 @@ function App() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                        <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -25,44 +26,37 @@ function App() {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-            <Route exact path="/">
-                <Jumbotron className="background">
-                    <h1>20% Season OFF</h1>
-                    <p>
-                        This is a simple hero unit, a simple jumbotron-style component for calling
-                        extra attention to featured content or information.
-                    </p>
-                    <p>
-                        <Button variant="primary">Learn more</Button>
-                    </p>
-                </Jumbotron>
-                <div className="container">
-                    <div className="row">
-                        {
-                            shoes.map((shoe) => {
-                                return (
-                                    <Card shoe={shoe} key={shoe.id}/>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </Route>
-            <Route path="/detail">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
-                        </div>
-                        <div className="col-md-6 mt-4">
-                            <h4 className="pt-5">상품명</h4>
-                            <p>상품설명</p>
-                            <p>120000원</p>
-                            <button className="btn btn-danger">주문하기</button>
+            <Switch>
+                <Route exact path="/">
+                    <Jumbotron className="background">
+                        <h1>20% Season OFF</h1>
+                        <p>
+                            This is a simple hero unit, a simple jumbotron-style component for calling
+                            extra attention to featured content or information.
+                        </p>
+                        <p>
+                            <Button variant="primary">Learn more</Button>
+                        </p>
+                    </Jumbotron>
+                    <div className="container">
+                        <div className="row">
+                            {
+                                shoes.map((shoe) => {
+                                    return (
+                                        <Card shoe={shoe} key={shoe.id}/>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
-                </div>
-            </Route>
+                </Route>
+                <Route path="/detail">
+                    <Detail/>
+                </Route>
+                <Route path="/:id">
+                    <div>아무거나 적었을때 보여주세요.</div>
+                </Route>
+            </Switch>
         </div>
     );
 }
